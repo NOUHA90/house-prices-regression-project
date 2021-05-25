86f2ddf834b3381ac231c8dcab55c898aa35f566
NOUHA90 <39261574+NOUHA90@users.noreply.github.com>
2021-05-25 17:17:52 +0200

    Modif2

---
+++ regression.py
@@ -0,0 +1,14 @@
+# Import library
+import pandas  as pd #Data manipulation
+import numpy as np #Data manipulation
+import matplotlib.pyplot as plt # Visualization
+import seaborn as sns #Visualization
+from IPython.display import Image
+
+
+class Regression:
+    
+    ### to be continued ;) ...
+
+    
+        
